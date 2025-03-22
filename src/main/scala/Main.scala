aee98187498be046a77ae193e49f92eac57e016b
Razvan <atomwalk12@gmail.com>
2025-03-22 20:38:58 +0100

    refactor: update project dependencies and structure
    - Replaced 'munit' with 'scalatest' in build.sbt for testing.
    - Updated main application to 'gitInspector' and removed previous hello world implementation.
    - Refactored test suite to use 'AnyFlatSpec' from 'scalatest' with a new test case.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import java.io.File
 
-def msg = "I was compiled by Scala 3. :)"
+
+@main
+def gitInspector(args: String*): Unit =
+  println("Git Inspector v0.1")
